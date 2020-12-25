ab8b83b85bea4d32d75fef4360745ee30a1cddb8
Rajesh <rajesh@caretit.com>
2020-12-25 17:13:20 +0530

    Added changes for the invoice pdf report

--- invoice_pdf_report/models/account_move_custom.py
+++ invoice_pdf_report/models/account_move_custom.py
@@ -8,11 +8,19 @@
 from odoo import models, fields, api, _
 
 
+
 class AccountMove(models.Model):
     _inherit = "account.move"
 
     purchase_order_number = fields.Char(string= 'Purchase Order No')
     projectname = fields.Text(string= 'Project Name')
     bank_account_id = fields.Many2one('res.partner.bank',string="Bank Account Number")
-    notes = fields.Text(string="Notes")
+    notes = fields.Text(string="Description")
+    is_contract =   fields.Boolean(string="For Contract?")
+    contract_order_number = fields.Char(string= 'Contract Order No')
 
+    def amount_to_text(self, amount, currency):
+        convert_amount_in_words = self.company_id.currency_id.amount_to_text(amount)
+        convert_amount_in_words = convert_amount_in_words.replace('And', '')
+        convert_amount_in_words = convert_amount_in_words.replace('Riyal', 'Saudi Riyals')
+        return convert_amount_in_words
