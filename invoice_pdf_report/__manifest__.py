861287be80c972b59de414da996d19054e34e36d
aashutosh <aashutosh@caretit.com>
2021-11-30 17:03:20 +0530

    Update menifest

--- invoice_pdf_report/__manifest__.py
+++ invoice_pdf_report/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     'name': 'Invoice PDF Report',
-    'version': '13.1.1.0',
+    'version': '13.1.1.1',
     'summary': 'Invoice PDF Custom Report',
     'category': '',
     'description': """
